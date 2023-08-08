f448c955dadd1c4867425d1092b223297c2b2b5f
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-08-08 16:26:40 +0300

    Update App.java

--- App.java
+++ App.java
@@ -1,12 +1,17 @@
-package vanar.projects;
+package vanar.projects.source;
 
 import java.io.IOException;
+
+import java.util.Map;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
-import vanar.projects.BNMSource.BNMSource;
+import vanar.projects.source.BNMSource.BNMSource;
+import vanar.projects.source.BNMSource.Valute;
+
+
 
 /**
  * Hello world!
@@ -20,7 +25,19 @@ public class App
     
 
         BNMSource bnms = new BNMSource();
-        bnms.getData();
+
+        Map<String, Valute> currencies = bnms.getData("01.08.2023");
+        currencies = bnms.getData("02.08.2023");
+        currencies = bnms.getData("03.08.2023");
+        currencies = bnms.getData("04.08.2023");
+        
+        
+
+
+
+       for (String key : currencies.keySet()) {
+        System.out.println(key + currencies.get(key));
+       }
 
     }
 }
